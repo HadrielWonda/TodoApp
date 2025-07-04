52841f0746cceb6b9a03be97861a6b0c71d5579b
HadrielWonda <olawalebash2017@gmail.com>
2025-07-04 15:17:30 +0100

    first commit

---
+++ Data/TodoService.cs
@@ -0,0 +1,47 @@
+using System.Collections.Concurrent;
+
+namespace TodoApp.Services;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+}
+
+public class TodoService
+{
+    private readonly ConcurrentDictionary<int, TodoItem> _todos = new();
+    private int _nextId = 1;
+
+    public Task<List<TodoItem>> GetTodosAsync() => 
+        Task.FromResult(_todos.Values.OrderBy(t => t.CreatedDate).ToList());
+
+    public Task AddTodoAsync(string title)
+    {
+        var todo = new TodoItem
+        {
+            Id = _nextId++,
+            Title = title
+        };
+        _todos[todo.Id] = todo;
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateTodoAsync(TodoItem updatedTodo)
+    {
+        if (_todos.TryGetValue(updatedTodo.Id, out var existing))
+        {
+            existing.Title = updatedTodo.Title;
+            existing.IsComplete = updatedTodo.IsComplete;
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteTodoAsync(int id)
+    {
+        _todos.TryRemove(id, out _);
+        return Task.CompletedTask;
+    }
+}
